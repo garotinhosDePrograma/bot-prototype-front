cf4d697c4c6300462cac633ae085ff0b6fb9068f
WirkLichKeit1 <luizfagnersilva87@gmail.com>
2026-02-05 14:21:07 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,14 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   compress: true,
   poweredByHeader: false,
   images: {
-    domains: ['https://bot-prototype.onrender.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'bot-prototype.onrender.com',
+      },
+    ],
     formats: ['image/avif', 'image/webp'],
   },
   async headers() {
@@ -36,4 +34,4 @@ const nextConfig = {
   },
 }
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
